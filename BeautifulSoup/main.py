86d8e4e410fbcb99fb1d211fbc89d4fa49061d05
Ignasius Silaban <92926515+Tior3turn@users.noreply.github.com>
2022-02-07 00:04:27 +0700

    Update main.py

--- BeautifulSoup/main.py
+++ BeautifulSoup/main.py
@@ -1,1 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
 
+get = requests.get("https://www.bmkg.go.id")
+bs = BeautifulSoup(get.text, 'html.parser')
+
+main = bs.find('section', id='meteorologi-geofisika')
+artikel = main.find_all(class_='carousel-block-table prakicu-kota')
+for title in artikel:
+    kota = title.find("h2", class_="kota")
+    cuaca = title.find("h2", class_="heading-md")
+    print(kota.text ,cuaca.text)
